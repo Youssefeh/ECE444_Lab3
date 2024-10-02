e9e94c67924c448491ba1626a5ed8f8145258d02
youssefeh <youssefelhaddad2011@gmail.com>
2024-10-02 15:02:47 -0400

    Ensure working createall db

--- project/app.py
+++ project/app.py
@@ -29,6 +29,7 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 url = os.getenv('DATABASE_URL', f'sqlite:///{Path(basedir).joinpath(DATABASE)}')
 if url.startswith('postgres://'):
+    print("hello")
     url = url.replace('postgres://', 'postgresql://', 1)
 
 SQLALCHEMY_DATABASE_URI = url
